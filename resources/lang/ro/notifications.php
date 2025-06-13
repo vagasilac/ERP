2c092915809ceeac6b14c754e72e0386915f529f
vagasilac <vagasilac@gmail.com>
2025-06-13 23:48:09 +0300

    Add files via upload

---
+++ resources/lang/ro/notifications.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    '<b>:user</b> a creat un proiect nou: <b>:project</b>' => '<b>:user</b> a creat un proiect nou: <b>:project</b>',
+    '<b>:user</b> a editat foaia de date cu statusul terminat/aprobat în cazul proiectului: <b>:project</b>' => '<b>:user</b> a editat foaia de date cu statusul terminat/aprobat în cazul proiectului: <b>:project</b>',
+    '<b>:user</b> a setat foaia de date ca și terminat în cazul proiectului: <b>:project</b>' => '<b>:user</b> a setat foaia de date ca și terminat în cazul proiectului: <b>:project</b>',
+];
---
+++ resources/lang/ro/suppliers.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    ':user a evaluat acest furnizor în data de :date' => ':user a evaluat acest furnizor în data de :date',
+];
